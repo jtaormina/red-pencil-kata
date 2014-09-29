a304a68ea616ae06863dabe3d3d7ba21be2cc044
jstaormina <jstaormina@gmail.com>
2014-09-29 15:38:47 -0400

    added promotion over if price reduced by >30% of original price

--- src/main/groovy/RedPencilCalculator.groovy
+++ src/main/groovy/RedPencilCalculator.groovy
@@ -1,18 +1,26 @@
 class RedPencilCalculator {
 	
 	def initialPrice
-	def currentPrice
+	def price
 	
 	public RedPencilCalculator(initialPrice) {
 		this.initialPrice = initialPrice
+		this.price = initialPrice
 	}
 	
-	public updatePrice(newPrice) {
-		currentPrice = newPrice	
+	public updatePrice(newPrice) { 
+		if (isThisOnPromotion(this.initialPrice, newPrice)) {
+			this.initialPrice = this.price
+		}
+		this.price = newPrice	
+	}
+	
+	private isThisOnPromotion(initialPrice, newPrice) {
+		(1-(newPrice/initialPrice)) >= 0.05 && (1-(newPrice/initialPrice)) <= 0.3
 	}
 	
 	public isOnPromotion() {
-		(1-(currentPrice/initialPrice)) >= 0.05 && (1-(currentPrice/initialPrice)) <= 0.3
-	}
+		this.isThisOnPromotion(this.initialPrice, this.price)
+	} 
 	
 }
--- src/test/groovy/RedPencilTest.groovy
+++ src/test/groovy/RedPencilTest.groovy
@@ -4,26 +4,43 @@ import org.junit.Test;
 
 
 class RedPencilTests extends GroovyTestCase {
-
+	
+	def calc
+	public void setUp() {
+		calc = new RedPencilCalculator(100)
+	}
+	
 	@Test
 	public void testPromotionBeginsAtFivePercent() {
-		RedPencilCalculator calc = new RedPencilCalculator(100)
 		calc.updatePrice(95)
 		assertTrue(calc.isOnPromotion())
 	}
 	
 	@Test
 	public void testPromotionDoesntBeginAtFourPercent() {
-		RedPencilCalculator calc = new RedPencilCalculator(100)
 		calc.updatePrice(96)
 		assertFalse(calc.isOnPromotion())
 	}
 
 	@Test
 	public void testPromotionDoesntOccurAtThirtyOnePercent() {
-		RedPencilCalculator calc = new RedPencilCalculator(100)
 		calc.updatePrice(69)
 		assertFalse(calc.isOnPromotion())
 	}
-
+	
+	@Test
+	public void testPromotionEndsPriceIsIncreased() {
+		calc.updatePrice(75)
+		assertTrue(calc.isOnPromotion())
+		calc.updatePrice(90)
+		assertFalse(calc.isOnPromotion())
+	}
+	
+	@Test
+	public void testPromotionEndsIfReducedBelowThirtyPercentOfOriginalPrice() {
+		calc.updatePrice(95)
+		assertTrue(calc.isOnPromotion())
+		calc.updatePrice(69)
+		assertFalse(calc.isOnPromotion())
+	}
 }
